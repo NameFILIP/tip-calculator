f75e4c9666e4a1fb0d762380928123b45a890f12
Filip Spiridonov <filip.spiridonov@gmail.com>
2023-10-09 00:18:34 -0400

    Fix the test

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "./app";
 
-test("renders learn react link", () => {
+test("renders Tip Calculator header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Tip Calculator/i);
+  expect(headerElement).toBeInTheDocument();
 });
